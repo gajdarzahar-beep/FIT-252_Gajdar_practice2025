187df9f81b387f72655ce6cd13ed618db4ad8a7d
gajdarzahar-beep <gajdarzahar.beep@gmail.com>
2026-06-25 11:24:33 +0600

    Задание 1: метод IsPalindrome и тесты

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        string lower = input.ToLower();
+        string clean = "";
+        foreach (char c in lower)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+            {
+                clean += c;
+            }
+        }
+
+        char[] cMassiv = clean.ToCharArray();
+        Array.Reverse(cMassiv);
+        string revers = new string(cMassiv);
+
+        return clean == revers;
+    }
+}
